630e9553cdbde4f71f7bec05bc0f609dd0d2e35b
lequangthong13 <lequangthong1996@gmail.com>
2024-09-18 15:47:40 +0300

    Add User Input in main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+Fi = str(input("Enter your first name: "))
+La = str(input("Enter your last name: "))
+Birthday = input("Enter your Birthday (dd/mm/yyyy)")
+print(f"Hello {Fi} {La}, welcome to the Motion Detector!\nLet's Start!!!\nYour Username is: {Fi[0:2]}{La[0:3]}")
+
+
+
+# input("Your first name: ")
+# input("Your last name: ")
+# from datetime import datetime
+# birthday = input("Enter your date of birth: ")
+# bday = datetime.strptime(birthday, '%d/%m/%Y')
+# print (bday)
